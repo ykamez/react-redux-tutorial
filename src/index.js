d4d18d36b4e9c6225cacc8ac1360bddd77549769
Yuta Kamezaki <25625303+ykamez@users.noreply.github.com>
2019-11-02 16:02:26 +0900

    Replace Square with function component

--- src/index.js
+++ src/index.js
@@ -2,25 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        // 親から渡されたonClickというメソッドをただ実行し、親に伝播する
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// Squareは自身のstateを持たないので、関数コンポーネントとして書くことができる
+// this.props ではなく、 props と書くのも特徴的。
+// 自分自身のstate を持たないシンプルなコンポーネントはこのように、関数コンポーネントを使っていくべきだし、そのようなものが多い状態が好ましいとykamezは考えた。
+function Square(props) {
+  return (
+    <button
+      // 親から渡されたonClickというメソッドをただ実行し、親に伝播する
+      className="square" onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
