0f4f786ab2d5e2d1ecad423a26000acfdf7dff3c
Yuta Kamezaki <25625303+ykamez@users.noreply.github.com>
2019-11-02 16:33:56 +0900

    Impl jumpTo

--- src/index.js
+++ src/index.js
@@ -57,11 +57,12 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     }
   }
 
   handleClick(i){
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     // NOTE: slice はコピーを作成する
     // イミュータブル性を保つために、このようにしている
@@ -75,12 +76,20 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -88,7 +97,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>
             {desc}
           </button>
